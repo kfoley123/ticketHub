3f987b2d21e9d530c28e42b17b373b4826796b26
Evan <kortney_foley@hotmail.com>
2025-03-23 16:10:30 -0300

    created required fields for models

--- TicketHub/Concert.cs
+++ TicketHub/Concert.cs
@@ -2,11 +2,13 @@
 {
     public class Concert
     {
-        public int Id { get; set; }
+        public int ConcertId { get; set; }
         //do i need any of this??? TODO
         //public string Artist { get; set; }
         //public string Venue { get; set; }
         //public DateOnly Date { get; set; }
         //public string? Summary { get; set; }
+
+        public List<TicketPurchase> TicketPurchases { get; set; } = new();
     }
 }
--- TicketHub/TicketPurchase.cs
+++ TicketHub/TicketPurchase.cs
@@ -1,20 +1,41 @@
-﻿namespace TicketHub
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TicketHub
 {
     public class TicketPurchase
     {
         public int Id { get; set; }
-        public int ConcertId { get; set; }
+        [Required, EmailAddress]
         public string? Email { get; set; }
+        [Required, MaxLength(255)]
         public string? Name { get; set; }
+        [Required, Phone]
         public string? Phone { get; set; }
+        [Required]
         public int Quantity { get; set; }
+        [Required, CreditCard]
         public string? CreditCard { get; set; }
+        [Required, MaxLength(4)]
         public string? Expiry { get; set; }
+        [Required]
         public string? SecurityCode { get; set; }
+        [Required]
         public string? Address { get; set; }
+        [Required]
         public string? City { get; set; }
+        [Required]
         public string? Province { get; set; }
+        [Required, MaxLength(6)]
         public string? PostalCode { get; set; }
-        public string? Country { get; set; } 
+        [Required]
+        public string? Country { get; set; }
+
+        //foreign key linking to Concert
+        [ForeignKey("Concert")]
+        public int ConcertId { get; set; }
+
+        //navigation property
+        public Concert Concert { get; set; }
     }
 }
